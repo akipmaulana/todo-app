18fbe8c6979863cb917a978ad6a7b628fe90ae9f
Akip Maulana <akip.maulana@gmail.com>
2018-09-17 20:38:23 +0700

    Add Splash Screen IOS

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import RNSplashScreen from 'react-native-splash-screen';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,8 +17,12 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+  componentDidMount() {
+    RNSplashScreen.hide()
+  }
+
   render() {
     return (
       <View style={styles.container}>
