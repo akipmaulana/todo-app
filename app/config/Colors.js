bb2dfc37b778a9c8a2fdedc50240c4170a2cc609
Akip Maulana <akip.maulana@gmail.com>
2018-09-19 15:26:34 +0700

    Add Config Colors and Settings

---
+++ app/config/Colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+    red: "#EF5350",
+    yellow: '#FFEE58',
+    amber: '#FFCA28',
+    blue: '#29B6F6',
+    orange: '#FF7043',
+    green: '#9CCC65',
+    light_grey1: '#F8F8F8', // Main Background
+    light_grey2: '#F2F2F2',
+    light_grey3: '#EDEDED',
+    light_grey4: '#C2C2C2',
+    dark_grey1: '#707070',
+    dark_grey2: '#4D4D4D',
+    white: '#FFFFFF',
+    black: '#000000',
+}
