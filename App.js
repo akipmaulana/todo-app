95908a37bf781b280fc2c9a740226af9b3e96cc4
Akip Maulana <akip.maulana@gmail.com>
2018-10-02 08:49:22 +0700

    With Main Navigation

--- App.js
+++ App.js
@@ -22,8 +22,7 @@ export default class App extends Component {
   render() {
     return (
         <Provider store={store}>
-            {/* <MainNavigation/> */}
-            <ProjectScreen/>
+            <MainNavigation/>
         </Provider>
     );
     
