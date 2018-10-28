16ab6fcab94671ecd192def1efc6fffa1a2c1fd8
Akip Maulana <akip.maulana@gmail.com>
2018-10-28 08:33:53 +0700

    Handle Loading Per Component

--- app/components/Cell/ProjectCell.js
+++ app/components/Cell/ProjectCell.js
@@ -1,37 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {Card, CardItem, Text, Body, View} from 'native-base';
 import Style from "./Style";
 import { ButtonClose, ButtonOpen } from 'components/Button'
 import { Color } from 'config'
 
-export const ProjectCell = props => {
+export class ProjectCell extends Component {
 
-    const { data, handleClosed, isCloseProject } = props
+    state = {
+        closeLoading: false
+    }
 
     handlePress = () => {
         const closeData = {
-            index: data.index,
-            id: data.item.id,
-            isClosed: !data.item.isClosed
+            index: this.props.data.index,
+            id: this.props.data.item.id,
+            isClosed: !this.props.data.item.isClosed
         }
-        handleClosed(closeData)
+        this.props.handleClosed(closeData)
+        this.setState({closeLoading: true})
     }
 
-    return (
-        <Card transparent style={Style.background_card}>
-            <CardItem style={Style.background_card}>
-                <Body>
-                    <View style={{...Style.badge_cell, backgroundColor: data.item.isClosed ? Color.light_grey4 : Color.amber}}/>
-                    <Text style={Style.flag_last_update}>Last Update</Text>
-                    <Text style={Style.time_text}>{ data.item.updatedAt }</Text>
-                    <Text style={Style.project_name_text}>{ data.item.name }</Text>
-                    {
-                        data.item.isClosed ?
-                            <ButtonOpen loading={isCloseProject} onPress={handlePress.bind(this)} /> :
-                            <ButtonClose loading={isCloseProject} onPress={handlePress.bind(this)} />
-                    }
-                </Body>
-            </CardItem>
-        </Card>
-    );
+    componentDidUpdate(prevProps) {
+        if (this.props.data.item.isClosed !== prevProps.data.item.isClosed) {
+            this.setState({ closeLoading: false })
+        }
+    }
+
+    render() {
+        return (
+            <Card transparent style={Style.background_card}>
+                <CardItem style={Style.background_card}>
+                    <Body>
+                        <View style={{...Style.badge_cell, backgroundColor: this.props.data.item.isClosed ? Color.light_grey4 : Color.amber}}/>
+                        <Text style={Style.flag_last_update}>Last Update</Text>
+                        <Text style={Style.time_text}>{ this.props.data.item.updatedAt }</Text>
+                        <Text style={Style.project_name_text}>{ this.props.data.item.name }</Text>
+                        {
+                            this.props.data.item.isClosed ?
+                                <ButtonOpen loading={this.state.closeLoading} onPress={this.handlePress.bind(this)} /> :
+                                <ButtonClose loading={this.state.closeLoading} onPress={this.handlePress.bind(this)} />
+                        }
+                    </Body>
+                </CardItem>
+            </Card>
+        );
+    }
 }
