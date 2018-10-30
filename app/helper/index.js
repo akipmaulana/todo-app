79d0a0d1b08385f012ba7886cb847e12ebf5ad92
Akip Maulana <akip.maulana@gmail.com>
2018-10-30 16:57:07 +0700

    Add Screen Helper

--- app/helper/index.js
+++ app/helper/index.js
@@ -1,4 +1,5 @@
 import * as GeneralHelper from './GeneralHelper';
 import * as ReduxHelper from './ReduxHelper';
+import * as ScreenHelper from './ScreenHelper';
 
-export {GeneralHelper, ReduxHelper}
+export {GeneralHelper, ReduxHelper, ScreenHelper}
